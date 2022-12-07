d5da928f04b112b6091fe97d7ceb17049f050cff
yuen <nicolaslopez@gmail.com>
2022-12-07 16:01:48 +0100

    added hamburger menu with temporary js file

---
+++ src/dist/index.485ab57d.js
@@ -0,0 +1,11 @@
+const bars = document.querySelector("#bars");
+const close = document.querySelector("#close");
+const nav = document.querySelector("#navbar");
+if (bars) bars.addEventListener("click", ()=>{
+    nav.classList.add("active");
+});
+if (close) close.addEventListener("click", ()=>{
+    nav.classList.remove("active");
+});
+
+//# sourceMappingURL=index.485ab57d.js.map
