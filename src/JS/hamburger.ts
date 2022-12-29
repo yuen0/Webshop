844f86a3ab4a8b153065ada5f4ef61a6e3ca7484
yuen <nicolaslopez@gmail.com>
2022-12-29 03:22:41 +0100

    added styling to cartpage

---
+++ src/JS/hamburger.ts
@@ -0,0 +1,19 @@
+const bars = document.querySelector("#bars");
+const closing = document.querySelector("#close");
+const nav = document.querySelector("#navbar");
+
+if (bars) {
+	bars.addEventListener("click", () => {
+		if (nav) {
+			nav.classList.add("active");
+		}
+	});
+}
+
+if (closing) {
+	closing.addEventListener("click", () => {
+		if (nav) {
+			nav.classList.remove("active");
+		}
+	});
+}
